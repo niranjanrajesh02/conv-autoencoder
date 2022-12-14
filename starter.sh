c9436ae8c2d8cbf219b3819266539fdabaf18ade
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2022-12-14 21:24:32 +0530

    shell script

---
+++ starter.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#PBS -N auto_encoder_tnbc
+#PBS -o out.log
+#PBS -e err.log
+#PBS -l ncpus=50
+#PBS -q cpu
+
+source /apps/compilers/anaconda3/etc/profile.d/conda.sh
+conda activate tnbc-general
+python /home/niranjan.rajesh_ug23/TNBC/conv-autoencoder/main.py
